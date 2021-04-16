fa514957071979f7250aeda02493482c20cba64e
Elaman Imashov <sosgames75743@gmail.com>
2021-04-16 15:34:01 +0600

    added some news

--- src/components/WorldNews/WorldNews.js
+++ src/components/WorldNews/WorldNews.js
@@ -4,6 +4,29 @@ const WorldNews = () => {
     return ( 
         <div className={classes.WorldNews}>
             <h1>This page Price</h1>
+            <p>Price List</p>
+            <div className={classes.big}>
+                <div className={classes.min}>
+                    <h2>Price 1</h2>
+                    <ul>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                    </ul>
+                </div>
+                <div className={classes.min}>
+                    <h2>Price 1</h2>
+                    <ul>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                        <li>product 1 10000</li>
+                    </ul>
+                </div>
+            </div>
             <img src="https://edit.org/img/blog/7pr-price-list-templates-edit-editable-personalize-free-easy-fast-print-bandamp-w-black-white-pricing.jpg" alt=""/>
         </div>
     );
